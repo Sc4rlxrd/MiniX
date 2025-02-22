61a2b82e952598431e0d33cf2cb3e499ff2453e7
Sc4rlxrd <guilhermesantos2004gss@gmail.com>
2025-02-22 15:59:27 -0300

    feat: first commit

---
+++ src/main/java/com/scarlxrd/minix/MinixApplication.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.minix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scarlxrd/minix/MinixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.minix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
